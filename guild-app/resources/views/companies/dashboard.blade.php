e4045ca57a675a55d6916983335b66387b45e20f
ujibayashi ryunosuke <ujibayashiryunosuke@ujibayashinoMacBook-Air.local>
2025-01-23 17:19:58 +0800

    working on the dashboard

--- guild-app/resources/views/companies/dashboard.blade.php
+++ guild-app/resources/views/companies/dashboard.blade.php
@@ -3,5 +3,12 @@
 @section('title', 'Dashboard')
 
 @section('content')
-    
+    <div class="row">
+        <div class="col-7">
+
+        </div>
+        <div class="col-5">
+            <img src="" alt="">
+        </div>
+    </div>
 @endsection
