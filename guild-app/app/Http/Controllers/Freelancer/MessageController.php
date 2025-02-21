af926a9bdd473a1744ef7195edc9706c753e03f0
Kenta <nojiken6767@gmail.com>
2025-02-21 14:05:54 +0800

    created filter

--- guild-app/app/Http/Controllers/Freelancer/MessageController.php
+++ guild-app/app/Http/Controllers/Freelancer/MessageController.php
@@ -10,7 +10,6 @@ use Illuminate\Support\Facades\Auth;
 
 
 
-use Illuminate\Http\Request;
 
 class MessageController extends Controller
 {
--- guild-app/resources/views/users/project-list.blade.php
+++ guild-app/resources/views/users/project-list.blade.php
@@ -7,53 +7,98 @@
 @endsection
 
 @section('scripts')
-<script src="{{asset('js/favorite-project.js')}}"></script>
+    <script src="{{asset('js/favorite-project.js')}}"></script>
 @endsection
 
 @section('content')
-    <div class="row justify-content-center job-list">
-        <div class="col-8">
-            <h2>JOB LIST</h2>
+    <div class="row  justify-content-center">
+        {{-- Filter --}}
+        <div class="col-3">
+            <div class="filter-container">
+                <h3>Filter Projects</h3>
+                <form  action="{{route('freelancer.project.index')}}" method="GET" class="filter-form">
+                    <!-- Keyword Searching -->
+                    <input type="text" name="keyword" placeholder="keyword" value="{{ request('keyword') }}">
 
-            {{-- Searching --}}
+                    <!-- Required Rank -->
+                    <label for="rank">Rank</label>
+                    <select name="required_rank">
+                        <option value="">Select Rank</option>
+                        @for ($i = 1; $i <= 5; $i++)
+                            <option value="{{ $i }}" {{ request('required_rank') == $i ? 'selected' : '' }}>
+                                {{ str_repeat('★', $i) }}
+                            </option>
+                        @endfor
+                    </select>
 
-            @foreach ($all_projects as $project)
-                <a href="{{route('freelancer.project-details', $project->id)}}" class="text-decoration-none text-dark">
-                    <div class="job">
-                        <div class="job-detail-container">
-                            <div class="job-date">{{$project->deadline}}</div>
-                            <div class="job-detail">
-                                <h4>{{$project->title}}</h4>
-                                <p class="fw-bold m-0">{{$project->company->user->name}}</p>
-                                <p class="m-0">${{$project->reward_amount}}</p>
-                                <p>
-                                    <?php
-                                        for($i = 1; $i <= $project->required_rank; $i++){
-                                    ?>
-                                            <i class="fa-solid fa-star"></i>
-                                    <?php
-                                        }
-                                    ?>
-                                </p>
+                    <!-- Skill -->
+                    <label for="rank">Skill</label>
+                    <select name="skills[]" class="select-skill" multiple>
+                        <option value="" hidden>Select Skills</option>
+                        @foreach($all_skills as $skill)
+                            <option value="{{ $skill->id }}" {{ in_array($skill->id, request('skills', [])) ? 'selected' : '' }}>
+                                {{ $skill->name }}
+                            </option>
+                        @endforeach
+                    </select>
 
-                                @foreach ($project->skills as $skill)
-                                    <span class="skill-tag">{{$skill->name}}</span>
-                                @endforeach
-                            </div>
-                        </div>
-                        <a class="favoriteBtn" data-url="{{route('freelancer.project.favorite', ['project' => $project->id])}}">
-                            <i class="fa-heart fa-2x {{ $project->isFavorited() ? 'fa-solid' : 'fa-regular' }}"></i>
-                        </a>
-                    </div>
-                </a>
-            @endforeach
+                    <!-- Reward Amount -->
+                    <label for="rank">Reward Amount</label>
+                    <input type="number" name="min_reward" placeholder="Min Reward" value="{{ request('min_reward') }}">
+                    <input type="number" name="max_reward" placeholder="Max Reward" value="{{ request('max_reward') }}">
 
+                    <!-- Latest or Oldest -->
+                    <label for="rank">Sort Order</label>
+                    <select name="sort">
+                        <option value="new" {{ request('sort', 'new') == 'new' ? 'selected' : '' }}>Latest</option>
+                        <option value="old" {{ request('sort') == 'old' ? 'selected' : '' }}>Oldest</option>
+                    </select>
 
-            {{-- paginate --}}
-            <div class="d-flex justify-content-start">
-                {{$all_projects->links()}}
+                    <button type="submit">Search</button>
+                </form>
             </div>
+        </div>
 
+        {{-- Project List --}}
+        <div class="col-8">
+            <div class="job-list">
+                <h2> PROJECT LIST</h2>
+                @foreach ($all_projects as $project)
+                    <a href="{{route('freelancer.project-details', $project->id)}}" class="text-decoration-none text-dark">
+                        <div class="job">
+                            <div class="job-detail-container">
+                                <div class="job-date">{{$project->deadline}}</div>
+                                <div class="job-detail">
+                                    <h4>{{$project->title}}</h4>
+                                    <p class="fw-bold m-0">{{$project->company->user->name}}</p>
+                                    <p class="m-0">${{$project->reward_amount}}</p>
+                                    <p>
+                                        <?php
+                                            for($i = 1; $i <= $project->required_rank; $i++){
+                                        ?>
+                                                <i class="fa-solid fa-star"></i>
+                                        <?php
+                                            }
+                                        ?>
+                                    </p>
+
+                                    @foreach ($project->skills as $skill)
+                                        <span class="skill-tag">{{$skill->name}}</span>
+                                    @endforeach
+                                </div>
+                            </div>
+                            <a class="favoriteBtn" data-url="{{route('freelancer.project.favorite', ['project' => $project->id])}}">
+                                <i class="fa-heart fa-2x {{ $project->isFavorited() ? 'fa-solid' : 'fa-regular' }}"></i>
+                            </a>
+                        </div>
+                    </a>
+                @endforeach
+
+                {{-- paginate --}}
+                <div class="d-flex justify-content-start">
+                    {{$all_projects->links()}}
+                </div>
+            </div>
         </div>
     </div>
 @endsection
